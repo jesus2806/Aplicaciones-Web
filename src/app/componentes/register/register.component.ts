98d76564d11e8bafbcbd109766cc7dd6d5417751
Jesus Soto <soleje2862004@gmail.com>
2024-02-15 14:54:22 -0600

    Actualizacion de login y registro

--- src/app/componentes/register/register.component.ts
+++ src/app/componentes/register/register.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  loginForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      fullname: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+      confirmPass: ['', [Validators.required]] // Cambio aquí
+    }, { validator: this.passwordMatchValidator });
+  }
+
+  passwordMatchValidator(group: FormGroup) {
+    const password = group.get('password')?.value;
+    const confirmPass = group.get('confirmPass')?.value; // Cambio aquí
+    return password === confirmPass ? null : { passwordMismatch: true };
+  }
+
+  get fullname() {
+    return this.loginForm.controls['fullname'];
+  }
+
+  get email() {
+    return this.loginForm.controls['email'];
+  }
+
+  get password() {
+    return this.loginForm.controls['password'];
+  }
+
+  get confirmPass() {
+    return this.loginForm.controls['confirmPass']; // Cambio aquí
+  }
 }
